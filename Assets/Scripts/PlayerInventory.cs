b731f26b6c2dec2f285a17ec7725178b161c5d65
dawisthames <dawisthames@gmail.com>
2025-10-27 23:20:02 +0700

    Inventory Slot complete

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInventory : MonoBehaviour
 {
@@ -25,7 +26,14 @@ public class PlayerInventory : MonoBehaviour
     [SerializeField] GameObject camera_prefab;
     [SerializeField] GameObject key1_prefab;
 
+    [Space(20)]
+    [Header("UI")]
+    [SerializeField] Image[] inventorySlotImage = new Image[9];
+    [SerializeField] Image[] inventoryBackgroundImage = new Image[9];
+    [SerializeField] Sprite emptySlotSprite;
+
     [SerializeField] Camera cam;
+    [SerializeField] GameObject pickUpItem_gameobject;
 
     private Dictionary<itemType, GameObject> itemSetActive = new Dictionary<itemType, GameObject>() { };
     private Dictionary<itemType, GameObject> itemInstantiate = new Dictionary<itemType, GameObject>() { };
@@ -48,14 +56,26 @@ public class PlayerInventory : MonoBehaviour
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
         RaycastHit hitInfo;
 
-        if(Physics.Raycast(ray, out hitInfo, playerReach) && Input.GetKey(pickItemKey))
+        if(Physics.Raycast(ray, out hitInfo, playerReach))
         {
             IPickable item = hitInfo.collider.GetComponent<IPickable>();
             if (item != null)
             {
-                inventoryList.Add(hitInfo.collider.GetComponent<ItemPickable>().itemScriptableObject.item_type);
-                item.PickItem();
+                pickUpItem_gameobject.SetActive(true);
+                if (Input.GetKey(pickItemKey))
+                {
+                    inventoryList.Add(hitInfo.collider.GetComponent<ItemPickable>().itemScriptableObject.item_type);
+                    item.PickItem();
+                }
             }
+            else
+            {
+                pickUpItem_gameobject.SetActive(false);
+            }
+        }
+        else
+        {
+            pickUpItem_gameobject.SetActive(false);
         }
         // Items throw
         if (Input.GetKeyDown(throwItemKey) && inventoryList.Count > 1)
@@ -63,12 +83,40 @@ public class PlayerInventory : MonoBehaviour
             Instantiate(itemInstantiate[inventoryList[selectedItem]], position: throwItem_gameobject.transform.position, new Quaternion());
             inventoryList.RemoveAt(selectedItem);
 
-            if(selectedItem != 0)
+            if (selectedItem != 0)
             {
                 selectedItem -= 1;
             }
             NewItemSelected();
         }
+        // UI
+
+        for (int i = 0; i < 8; i++)
+        {
+            if (i < inventoryList.Count)
+            {
+                inventorySlotImage[i].sprite = itemSetActive[inventoryList[i]].GetComponent<Item>().itemScriptableObject.item_sprite;
+            }
+            else
+            {
+                inventorySlotImage[i].sprite = emptySlotSprite;
+            }
+        }
+
+        int a = 0;
+        foreach(Image image in inventoryBackgroundImage)
+        {
+            if(a == selectedItem)
+            {
+                image.color = new Color32(145, 255, 126, 255);
+            }
+            else
+            {
+                image.color = new Color32(219, 219, 219, 255);
+            }
+            a++;
+        }
+
 
         if (Input.GetKeyDown(KeyCode.Alpha1) && inventoryList.Count > 0)
         {
